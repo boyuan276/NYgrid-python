434461d8091b2304648028d718d7131771986a25
Bo Yuan <by276@cornell.edu>
2023-12-26 23:39:58 -0500

    Add pre and post processing functions

---
+++ nygrid/postprocessing.py
@@ -0,0 +1,6 @@
+"""
+Post-processing functions for NY grid data.
+
+Created: 2023-12-26, by Bo Yuan (Cornell University)
+Last modified: 2023-12-26, by Bo Yuan (Cornell University)
+"""
---
+++ nygrid/preprocessing.py
@@ -0,0 +1,6 @@
+"""
+Preprocessing functions for NY grid data.
+
+Created: 2023-12-26, by Bo Yuan (Cornell University)
+Last modified: 2023-12-26, by Bo Yuan (Cornell University)
+"""

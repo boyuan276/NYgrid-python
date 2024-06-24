be3a0df3bee38d8337cdd73679b39d3261eea8bd
Bo Yuan <by276@cornell.edu>
2024-06-24 19:51:07 -0400

    Refactor code to include pandas datetime conversion

--- nygrid/preprocessing.py
+++ nygrid/preprocessing.py
@@ -5,7 +5,12 @@ Created: 2023-12-26, by Bo Yuan (Cornell University)
 Last modified: 2023-12-26, by Bo Yuan (Cornell University)
 """
 
-def agg_demand_county2bus(demand_inc_county, county2bus):
+import os
+import pandas as pd
+from typing import List, Tuple, Optional
+
+def agg_demand_county2bus(demand_inc_county: pd.DataFrame, 
+                          county2bus: pd.DataFrame) -> pd.DataFrame:
     """
     County-level consumption to bus-level consumption
     """
@@ -38,3 +43,77 @@ def agg_demand_county2bus(demand_inc_county, county2bus):
     demand_inc_bus = demand_inc_bus.add(demand_inc_bus_westchester, fill_value=0)
 
     return demand_inc_bus
+
+
+def get_res_load_change_county(county_id: str, 
+                               upgrade_id: int, 
+                               res_bldg_type_list: List[str], 
+                               resstock_bldg_proc_dir: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+
+    Read building timeseries data aggregated by county and building type
+
+    Parameters
+    ----------
+    county_id : str
+        County ID
+    upgrade_id : int
+        Upgrade ID
+    res_bldg_type_list : list
+        List of building types
+    resstock_bldg_proc_dir : str
+        Directory for processed building data
+
+    Returns
+    -------
+    df_county_base: pd.DataFrame
+        Dataframe with baseline energy consumption
+    df_county_future: pd.DataFrame
+        Dataframe with future energy consumption
+    df_county_saving: pd.DataFrame
+        Dataframe with energy savings
+    """
+
+    # Read building timeseries data aggregated by county and building type
+    first_df = True
+
+    for bldg_type in res_bldg_type_list:
+
+        filename = os.path.join(resstock_bldg_proc_dir, 
+                                f"{county_id.lower()}_{bldg_type.replace(' ', '_').lower()}.parquet")
+        
+        if os.path.isfile(filename):
+            # Future
+            df_county_bldg_type_future = pd.read_parquet(filename, engine='pyarrow')
+            col_total_cons = [col for col in df_county_bldg_type_future.columns if col.endswith('.energy_consumption') and 'total' in col]
+            df_county_bldg_type_future = df_county_bldg_type_future[col_total_cons]
+
+            # Baseline
+            df_county_bldg_type_base = pd.read_parquet(filename.replace(f'upgrade={upgrade_id}', 'upgrade=0'), engine='pyarrow')
+            df_county_bldg_type_base = df_county_bldg_type_base[col_total_cons]
+
+            # Savings = Baseline - Future
+            df_county_bldg_type_saving = df_county_bldg_type_base - df_county_bldg_type_future
+
+            # Add to county saving dataframe
+            if first_df:
+                df_county_base = df_county_bldg_type_base
+                df_county_future = df_county_bldg_type_future
+                df_county_saving = df_county_bldg_type_saving
+                first_df = False
+            else:
+                df_county_base = df_county_base + df_county_bldg_type_base
+                df_county_future = df_county_future + df_county_bldg_type_future
+                df_county_saving = df_county_saving + df_county_bldg_type_saving
+            
+        else:
+            print(f'Residential load data is not available for county {county_id} {bldg_type}. Skipping...')
+            continue
+
+    # Rename columns
+    col_rename = {col: col.split('.')[1] for col in df_county_base.columns}
+    df_county_base = df_county_base.rename(columns=col_rename)
+    df_county_future = df_county_future.rename(columns=col_rename)
+    df_county_saving = df_county_saving.rename(columns=col_rename) 
+
+    return df_county_base, df_county_future, df_county_saving

58d301767fffa9218433ecd52c745b2fefe158db
Bo Yuan <by276@cornell.edu>
2024-11-30 01:57:52 -0500

    Add 2030 baseline OPF simulation script and adjust generation cost penalties

--- examples/03_opf_2030ContractCase_daily.py
+++ examples/03_opf_2030ContractCase_daily.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     # NOTE: Change the following settings to run the simulation
     ext_cost_factor = 0.0
     fo_cost_factor = 0.5
-    sim_name = f'2018NewParams_ext{ext_cost_factor}_fo{fo_cost_factor}_daily'
+    sim_name = f'2030ContractCase_ext{ext_cost_factor}_fo{fo_cost_factor}_daily'
 
     # Simulation time settings
     valid_days = 30
@@ -36,7 +36,8 @@ if __name__ == '__main__':
 
     sim_start_time = datetime(2018, 1, 1, 0, 0, 0)
     sim_end_time = datetime(2018, 12, 31, 23, 0, 0)
-    timestamp_list = pd.date_range(sim_start_time, sim_end_time, freq=f'{valid_days}D')
+    timestamp_list = pd.date_range(
+        sim_start_time, sim_end_time, freq=f'{valid_days}D')
     verbose = True
 
     if 'examples' in os.getcwd():
@@ -55,7 +56,7 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     data_dir = os.path.join(cwd, 'data')
 
-    grid_data_dir = os.path.join(data_dir, 'grid', '2018NewParams')
+    grid_data_dir = os.path.join(data_dir, 'grid', '2030ContractCase')
     if not os.path.exists(grid_data_dir):
         raise FileNotFoundError('Grid data directory not found.')
 
@@ -77,7 +78,10 @@ if __name__ == '__main__':
     # %% Read grid data
 
     # Read grid property file
-    grid_prop = ng_run.read_grid_prop(grid_data_dir)
+    grid_prop = ng_run.read_grid_prop(grid_data_dir,
+                                      if_lims_prop_file='if_lims_prop_2030ContractCase.csv',
+                                      esr_prop_file='esr_prop_2030ContractCase.csv',
+                                      dcline_prop_file='dcline_prop_2030ContractCase.csv')
 
     # Read load and generation profiles
     grid_profile = ng_run.read_grid_profile(grid_data_dir, sim_start_time.year)
@@ -86,7 +90,7 @@ if __name__ == '__main__':
 
     # Decrease external generation cost by 50%
     change_index = grid_prop["gen_prop"]["GEN_ZONE"].isin(
-        ['PJM','IESO', 'NE']).to_numpy()
+        ['PJM', 'IESO', 'NE']).to_numpy()
 
     gencost1_profile_new = grid_profile['gencost1_profile'].copy()
     gencost1_profile_new.loc[:, change_index] = \
@@ -96,10 +100,10 @@ if __name__ == '__main__':
     # Decrease FO2, KER and FO6 generation costs
     change_index = grid_prop["gen_fuel"]["GEN_FUEL"].isin(
         [
-            "CT_FO2", 
-            "CT_KER", 
+            "CT_FO2",
+            "CT_KER",
             "ST_FO6"
-         ]).to_numpy()
+        ]).to_numpy()
 
     gencost0_profile_new = grid_profile['gencost0_profile'].copy()
     gencost0_profile_new.loc[:, change_index] = \
@@ -111,7 +115,8 @@ if __name__ == '__main__':
         gencost1_profile_new.loc[:, change_index] * fo_cost_factor
     grid_profile['gencost1_profile'] = gencost1_profile_new
 
-    gencost_startup_profile_new = grid_profile['gencost_startup_profile'].copy()
+    gencost_startup_profile_new = grid_profile['gencost_startup_profile'].copy(
+    )
     gencost_startup_profile_new.loc[:, change_index] = \
         gencost_startup_profile_new.loc[:, change_index] * fo_cost_factor
     grid_profile['gencost_startup_profile'] = gencost_startup_profile_new
@@ -134,8 +139,8 @@ if __name__ == '__main__':
     }
 
     # No initial condition for the first day
-    last_gen = np.zeros(grid_prop['gen_prop'].shape[0]+
-                        grid_prop['esr_prop'].shape[0]+
+    last_gen = np.zeros(grid_prop['gen_prop'].shape[0] +
+                        grid_prop['esr_prop'].shape[0] +
                         grid_prop['dcline_prop'].shape[0]*2)
     last_gen_cmt = np.zeros(sum(grid_prop['gen_prop']['CMT_KEY'] == 1))
     last_soc = None
@@ -150,7 +155,8 @@ if __name__ == '__main__':
         cycle_start_time = timestamp_list[d]
 
         if d < len(timestamp_list) - 1:
-            cycle_end_time = cycle_start_time + timedelta(hours=valid_hours + lookahead_hours)
+            cycle_end_time = cycle_start_time + \
+                timedelta(hours=valid_hours + lookahead_hours)
         else:
             cycle_end_time = sim_end_time
 
@@ -175,25 +181,30 @@ if __name__ == '__main__':
 
         if d < len(timestamp_list) - 1:
             # Set initial conditions for the next iteration
-            time_before_next_cycle = cycle_start_time + timedelta(hours=valid_hours-1)
-            
+            time_before_next_cycle = cycle_start_time + \
+                timedelta(hours=valid_hours-1)
+
             # Set generator initial condition
-            last_gen = nygrid_results['PG'].loc[time_before_next_cycle].to_numpy().squeeze()
-            
+            last_gen = nygrid_results['PG'].loc[time_before_next_cycle].to_numpy(
+            ).squeeze()
+
             # Set generator commitment initial condition
-            last_gen_cmt = nygrid_results['genCommit'].loc[time_before_next_cycle].to_numpy().squeeze()
-            
+            last_gen_cmt = nygrid_results['genCommit'].loc[time_before_next_cycle].to_numpy(
+            ).squeeze()
+
             # Set ESR initial condition
-            last_soc = nygrid_results['esrSOC'].loc[time_before_next_cycle].to_numpy().squeeze()
+            last_soc = nygrid_results['esrSOC'].loc[time_before_next_cycle].to_numpy(
+            ).squeeze()
 
             # Calculate hours since last startup and shutdown
             hour_since_last_startup = ng_run.get_last_startup_hour(nygrid_results,
-                                                                    time_before_next_cycle)
+                                                                   time_before_next_cycle)
             hour_since_last_shutdown = ng_run.get_last_shutdown_hour(nygrid_results,
-                                                                    time_before_next_cycle)
-        
+                                                                     time_before_next_cycle)
+
         elapsed = time.time() - t
-        logging.info(f'Finished running for {cycle_start_time.strftime("%Y-%m-%d")}.')
+        logging.info(
+            f'Finished running for {cycle_start_time.strftime("%Y-%m-%d")}.')
         logging.info(f'Elapsed time: {elapsed:.2f} seconds')
         logging.info('-' * 80)
 

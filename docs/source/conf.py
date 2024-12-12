45c32798ee8d87a4ffdce7959f03268012c0024b
Bo Yuan <by276@cornell.edu>
2024-12-12 16:32:04 -0500

    Add Sphinx documentation setup with Makefile and configuration files

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NYgrid-python'
+author = 'Bo Yuan'
+release = 'v0.1.1'
+
+from datetime import datetime
+year = datetime.now().year
+if year > 2023:
+    copyright = f'2023-{year}, {author}'
+else:
+    copyright = f'{year}, {author}'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',      # Automatically generate docs from docstrings
+    'sphinx.ext.napoleon',    # Support for Google/NumPy style docstrings
+    'sphinx.ext.viewcode',    # Add links to source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. NYgrid-python documentation master file, created by
+   sphinx-quickstart on Thu Dec 12 16:17:31 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to NYgrid-python's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
